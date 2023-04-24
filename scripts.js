133f242ad0fec4533891dee3b3a1d19c2f9d5d67
ruudkas <kassidyruud@gmail.com>
2023-04-23 21:51:38 -0500

    fix parcel

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-import * as THREE from "../node_modules/three/build/three.module.js";
+import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer';
 import * as speeds from "./src/js/constants/speedConstants.js";
